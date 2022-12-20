f539fdd908363683efb8269cb1f0f8a05d3c8a24
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-12-21 02:54:27 +0900

    iuse state added

--- reat-meme-generator/src/components/meme/Meme.jsx
+++ reat-meme-generator/src/components/meme/Meme.jsx
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import memesData from "./memesData";
 import "./meme.css";
 
 const Meme = () => {
+  const [memeimage, setmemeimage] = useState("");
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const rng = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[rng].url;
-    console.log(url);
+    setmemeimage(memesArray[rng].url);
   }
   return (
-    <form className="generatorForm">
-      <input
-        className="input"
-        type="text"
-        id="top-txt"
-        name="top-txt"
-        placeholder="Top text"
-      />
-      <input
-        className="input"
-        type="text"
-        id="bot-txt"
-        name="bot-txt"
-        placeholder="Bottom text"
-      />
-      <input
-        type="button"
-        value="Generate!"
-        className="btn submit"
-        onClick={getMemeImage}
-      />
-    </form>
+    <>
+      <form className="generatorForm">
+        <input
+          className="input"
+          type="text"
+          id="top-txt"
+          name="top-txt"
+          placeholder="Top text"
+        />
+        <input
+          className="input"
+          type="text"
+          id="bot-txt"
+          name="bot-txt"
+          placeholder="Bottom text"
+        />
+        <input
+          type="button"
+          value="Generate!"
+          className="btn submit"
+          onClick={getMemeImage}
+        />
+      </form>
+      <img src={memeimage} alt="" className="memeImg" />
+    </>
   );
 };
 
